d741967b499b74c942690e050a28c8dd3d5d6bd1
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-25 15:14:01 -0300

    create api

---
+++ src/api/useBoardExists.ts
@@ -0,0 +1,35 @@
+import { fetcher } from "./fetcher";
+import { useCallback, useState } from "react";
+import { useSWRConfig } from "swr";
+
+export function useBoardExists() {
+  const { cache, mutate } = useSWRConfig();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const checkBoardExistence = useCallback(
+    async (board: string) => {
+      const key = `useBoardExists-${board}`;
+
+      if (cache.get(key)) return cache.get(key)?.data;
+
+      setIsLoading(true);
+
+      try {
+        const response = await fetcher(`/${board}.json`);
+        const exists = response?.error !== 404;
+
+        await mutate(key, exists, false);
+
+        return exists;
+      } catch (error) {
+        alert("Error checking board existence.");
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [cache, mutate]
+  );
+
+  return { isLoading, checkBoardExistence };
+}
