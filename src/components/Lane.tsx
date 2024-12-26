f6a2e4a40106185e7a9abf68823e4c3158e075d8
felipeog <17603069+felipeog@users.noreply.github.com>
2024-12-26 20:06:54 -0300

    maintain item scroll when moved

--- src/components/Lane.tsx
+++ src/components/Lane.tsx
@@ -7,6 +7,7 @@ import { CSS } from "@dnd-kit/utilities";
 import { useBoardContent } from "../api/useBoardContent";
 import { useBoardContext } from "../context/useBoardContext";
 import { defaultAnimateLayoutChanges, useSortable } from "@dnd-kit/sortable";
+import { useEffect, useRef } from "react";
 
 type TLaneProps = {
   board: string;
@@ -14,21 +15,46 @@ type TLaneProps = {
 };
 
 export function Lane(props: TLaneProps) {
+  const wrapperRef = useRef<HTMLElement>();
+  const scrollRef = useRef(0);
   const { boardContext } = useBoardContext();
   const { boardContent, isLoading, loadMore, refresh } = useBoardContent(
     props.board
   );
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({
-      id: props.board,
-      animateLayoutChanges: (args) =>
-        defaultAnimateLayoutChanges({ ...args, wasDragging: true }),
-    });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isSorting,
+  } = useSortable({
+    id: props.board,
+    animateLayoutChanges: (args) =>
+      defaultAnimateLayoutChanges({ ...args, wasDragging: true }),
+  });
+
+  function setRef(wrapper: HTMLElement | null) {
+    if (!wrapper) return;
+
+    wrapperRef.current = wrapper;
+    setNodeRef(wrapper);
+  }
+
+  useEffect(() => {
+    if (!wrapperRef.current) return;
+
+    if (isSorting) {
+      scrollRef.current = wrapperRef.current.scrollTop;
+    } else {
+      wrapperRef.current.scrollTop = scrollRef.current;
+    }
+  }, [isSorting]);
 
   return (
     <div
       className="Lane w-[30vw] min-w-[350px] h-full bg-base-100 border-r border-neutral-300 dark:border-neutral-700 overflow-y-scroll"
-      ref={setNodeRef}
+      ref={setRef}
       style={{
         zIndex: props.isDragging ? 10 : 0,
         transform: CSS.Transform.toString(transform),
